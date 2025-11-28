d4de4c30865555f3314befdfaf497e79ee2bc6dc
JradChaima1 <chaimajrad1888@gmail.com>
2025-11-28 19:47:05 +0000

    update Program.cs file

--- SchoolManagement/Program.cs
+++ SchoolManagement/Program.cs
@@ -1,15 +1,27 @@
+using Microsoft.EntityFrameworkCore;
+using School.Data;
+using School.Data.Repositories;
+using School.Core.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<SchoolContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+builder.Services.AddScoped<IStudentRepository, StudentRepository>();
+builder.Services.AddScoped<ITeacherRepository, TeacherRepository>();
+builder.Services.AddScoped<IClassroomRepository, ClassroomRepository>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
